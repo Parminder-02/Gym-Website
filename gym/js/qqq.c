f4890ab679ed852e5982ce45d0bbb8593ae7ec17
Parminder-02 <parmindersingh02112@gmail.com>
2025-08-20 23:02:02 +0530

    GYM

---
+++ gym/js/qqq.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+ void traverse(struct Node* head){
+    struct Node * current = head;
+ while(current!=NULL){
+printf("%d",current->data);
+current= current->next;
+ }
+ }
+int main(){
+    struct Node* head=NULL;
+    struct Node* second= NULL;
+    struct Node* third= NULL;
+
+    head=(struct Node*)malloc(sizeof(struct Node));
+    second=(struct Node*)malloc(sizeof(struct Node));
+    third =(struct Node*)malloc(sizeof(struct Node));
+
+    head->data =10;
+    head->next=second; 
+    second->data=20;
+    second->next=third;
+    third->data=30;
+    third-> next= NULL;
+
+    traverse(head);
+
+}
